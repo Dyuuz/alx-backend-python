a99ea25514dfae3b55aff27007e267eec6fbd7ac
Dyuuz <lekanoyesunle@gmail.com>
2025-11-09 16:21:37 +0100

    feat(async): run concurrent DB queries using asyncio.gather

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,23 @@
+import aiosqlite, asyncio
+
+db_file_path = "user.db"
+
+async def async_fetch_users():
+    async with aiosqlite.connect(database=db_file_path) as db:
+        async with db.execute("SELECT * FROM user") as cursor:
+               return await cursor.fetchall()
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect(database=db_file_path) as db:
+        async with db.execute("SELECT * FROM user WHERE age > 40") as cursor:
+            return await cursor.fetchall()
+
+async def fetch_concurrently():
+    return await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+        )
+
+users, older_users = asyncio.run(fetch_concurrently())
+print(f"All users: ", users)
+print("Older users: ", older_users)
