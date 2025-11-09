1463174fa9029ac99eed07e7cc2b04ea9768c04e
Dyuuz <lekanoyesunle@gmail.com>
2025-11-09 16:17:20 +0100

    feat(db): add class-based context manager for database connections

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+db_file_path = "user.db"
+
+class DatabaseConnection:
+
+    def __init__(self):
+        self.conn = sqlite3.connect(db_file_path)
+
+    def __enter__(self):
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.conn.close()
+        print(exc_type, exc_value, exc_tb, sep="\n")
+
+with DatabaseConnection() as connection:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    print(cursor.fetchall())
