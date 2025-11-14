382751b4350d406646ce4cc25f590e062db87271
Dyuuz <lekanoyesunle@gmail.com>
2025-11-14 20:59:39 +0100

    feat: test access nested map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,34 +4,25 @@ from parameterized import parameterized
 from typing import Mapping, Dict, Sequence, Any
 
 
-def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """Access nested map with key path.
-    Parameters
-    ----------
-    nested_map: Mapping
-        A nested map
-    path: Sequence
-        a sequence of key representing a path to the value
-    Example
-    -------
-    >>> nested_map = {"a": {"b": {"c": 1}}}
-    >>> access_nested_map(nested_map, ["a", "b", "c"])
-    1
-    """
-    for key in path:
-        if not isinstance(nested_map, Mapping):
-            raise KeyError(key)
-        nested_map = nested_map[key]
-
-    return nested_map
-
-
 class TestAccessNestedMap(unittest.TestCase):
-    @parameterized.expand([
-        ({"a": 1},  ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
+    """
+    Tests for the access_nested_map function.
+    """
 
-    def test_access_nested_map(self, input:Mapping[str, int], path:Sequence, expected):
-        self.assertEqual(access_nested_map(input, path), expected)       
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
+        """Test normal access of nested maps."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence, expected_exception):
+        """Test access that raises KeyError."""
+        with self.assertRaises(expected_exception):
+            access_nested_map(nested_map, path)     
