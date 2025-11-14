6cb81d44e199815a4aa126cea15003de3a839199
Dyuuz <lekanoyesunle@gmail.com>
2025-11-14 21:37:44 +0100

    add: test client with dependencies

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Unittests and integration tests for client.GithubOrgClient."""
+import unittest
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock
+import client
+from client import GithubOrgClient
+import fixtures
+import utils
+
+
+
+
+
+
